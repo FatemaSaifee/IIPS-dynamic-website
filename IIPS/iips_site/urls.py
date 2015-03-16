32af7a4ff17f05e5768109b1a6b2b307ecbe7d2b
FatemaSaifee <fatema.saifee711@gmail.com>
2015-03-16 22:37:13 +0530

    index.html,css,js,fonts added

--- IIPS/iips_site/urls.py
+++ IIPS/iips_site/urls.py
@@ -1,7 +1,23 @@
 from django.conf.urls import patterns, url
 from iips_site import views
 
+#get all model on one template
+#option1
+"""
+from django.views.generic import list_detail
+urlpatterns = patterns('',
+	(r'^$', list_detail.object_list, all_models_dict),
+)
+"""
 
+#option2
+
+"""
+url(r'^$', 
+    IndexView.as_view(),
+    name="home_list"
+        ),
+"""
 urlpatterns = patterns('',
      url(r'^$', views.IndexView.as_view(), name='index'),
      url(r'^gallary/$', views.GallaryView.as_view(), name='gallary'),
--- IIPS/iips_site/views.py
+++ IIPS/iips_site/views.py
@@ -8,7 +8,35 @@ from django.utils import timezone
 
 # Create your views here.
 
+#get all model on one template
+#option 1
+"""
+all_models_dict = {
+        "template_name": "contacts/index.html",#"iips_site/index.html"
+        "queryset": Individual.objects.all(),#News.objects.all(),
+        "extra_context" : {"role_list" : Role.objects.all(),#"gallary_list" : Gallary.objects.all(),
+                           "venue_list": Venue.objects.all(),# "course_list" :  Course.objects.all(),
+                           #and so on for all the desired models...
+                           }
+    }
+"""
 
+#option2
+
+"""
+class IndexView(ListView):
+context_object_name = 'home_list'    
+template_name = 'contacts/index.html'
+queryset = Individual.objects.all()
+
+def get_context_data(self, **kwargs):
+    context = super(IndexView, self).get_context_data(**kwargs)
+    context['roles'] = Role.objects.all()
+    context['venue_list'] = Venue.objects.all()
+    context['festival_list'] = Festival.objects.all()
+    # And so on for more models
+        return context
+"""
 
 class IndexView(generic.ListView):
     template_name = 'iips_site/index.html'
@@ -48,4 +76,6 @@ class Staff_infoView():
 
 def example_bootstrap_toolkit(request):
 	return render(request,'example_bootstrap_toolkit.html')
+
+
 
