92b994f2bc5edf65b4c5011a89e2d017a060388b
FatemaSaifee <fatema.saifee711@gmail.com>
2015-03-26 14:09:59 +0530

    programs.html linked with view successfully

--- IIPS/iips_site/views.py
+++ IIPS/iips_site/views.py
@@ -6,6 +6,7 @@ from django.views import generic
 from django.utils import timezone
 
 from django.views.generic.list import ListView
+from django.views.generic.detail import SingleObjectMixin
 # Create your views here.
 
 
@@ -61,13 +62,32 @@ class AdmissionView(ListView):
         return Admission.objects.all()
 
 class ProgramView(ListView):
-    context_object_name= 'item_list'
+    model = Program
     template_name = 'iips_site/program.html'
 
     def get_queryset(self):
         return Program.objects.all()
 
 
+
+class ProgramDetailView(SingleObjectMixin, ListView):
+    #paginate_by = 2
+    template_name = "iips_site/programdetail.html"
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset=Program.objects.all())
+        return super(ProgramDetailView, self).get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(ProgramDetailView, self).get_context_data(**kwargs)
+        context['program'] = self.object
+        context['program_list'] = Program.objects.all()
+
+        return context
+
+    def get_queryset(self):
+        return self.object.course_set.all()
+'''
 
 class ProgramDetailView(generic.DetailView):
     model = Program
@@ -76,6 +96,7 @@ class ProgramDetailView(generic.DetailView):
     def get_queryset(self):
         
         return Program.objects.all()
+'''
 
 class NewsView(ListView):
     context_object_name= 'item_list'
